e27e2b5d7e6026150a63f55d707cc0b9ae3dff1d
Dmytro Zubenko <fokist@outlook.com>
2023-04-16 22:05:24 +0300

    Failed tests screenshot maker implemented

--- src/test/java/browserConfiguration/BaseTest.java
+++ src/test/java/browserConfiguration/BaseTest.java
@@ -3,9 +3,16 @@ package browserConfiguration;
 import com.microsoft.playwright.Page;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.api.extension.TestWatcher;
 import playwrightInitializer.PlaywrightFactory;
 
-public class BaseTest {
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class BaseTest implements TestWatcher {
     static PlaywrightFactory playwrightFactory;
     protected static Page page;
 
@@ -14,6 +21,18 @@ public class BaseTest {
         playwrightFactory = new PlaywrightFactory();
         page = playwrightFactory.initialiseBrowser("chrome");
     }
+
+
+    @RegisterExtension
+    public TestWatcher watchman= new TestWatcher() {
+        @Override
+        public void testFailed(ExtensionContext context, Throwable cause) {
+            SimpleDateFormat df = new SimpleDateFormat("dd.MM.yyyy~HH-mm-ss");
+            String timeStamp = df.format(new Date());
+            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("target/failedTestsScreenshots/TestFailed" + timeStamp + ".png")).setFullPage(true));
+
+        }
+    };
 
     @AfterAll
     static void tearDown() {
